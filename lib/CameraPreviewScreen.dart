deb7524e4f32d86febb6f0ee48dd0a1fc6e0f939
Hariom <patelhariomk@gmail.com>
2023-03-17 01:51:22 +0530

    camera implemented

---
+++ lib/CameraPreviewScreen.dart
@@ -0,0 +1,46 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/cupertino.dart';
+
+class CameraPreviewScreen extends StatefulWidget {
+  @override
+  _CameraPreviewScreenState createState() => _CameraPreviewScreenState();
+}
+
+class _CameraPreviewScreenState extends State<CameraPreviewScreen> {
+  late CameraController controller;
+  late List<CameraDescription> cameras;
+
+  @override
+  void initState() {
+    startCamera();
+    super.initState();
+  }
+
+  void startCamera() async {
+    cameras = await availableCameras();
+    controller = CameraController(cameras[0], ResolutionPreset.medium);
+    controller.initialize().then((_) {
+      if (!mounted) {
+        return;
+      }
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (!controller.value.isInitialized) {
+      return Container();
+    }
+    return AspectRatio(
+      aspectRatio: controller.value.aspectRatio,
+      child: CameraPreview(controller),
+    );
+  }
+}
